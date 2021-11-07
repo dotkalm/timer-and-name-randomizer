1c4f97e8bf77696ec73d8b8512c99317efcd8c6c
Joel Holmberg <joelmholmberg@gmail.com>
2021-11-06 21:16:20 -0700

    rename parameter for randomizer

--- utils/randomizeNames.ts
+++ utils/randomizeNames.ts
@@ -1,4 +1,4 @@
-export default function randomize(names: string[]): string{
+export default function randomize(array: string[]): string{
 	const r = Math.floor(Math.random() * array.length)
 	const [ element ] = array.splice(r, 1)
 	return element
